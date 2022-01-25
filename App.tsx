56575791d6b50d2056f95f945cce14448bb733c8
RahulGoyal-tech <rhgoyal01@gmail.com>
2022-01-25 22:43:00 +0530

    Removed Bug: UI not updating on deletion

--- App.tsx
+++ App.tsx
@@ -1,63 +1,88 @@
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SwipeListView } from 'react-native-swipe-list-view';
-
-let i = 0
 
 const App: React.FC<{}> = () => {
 
-  i++;
-  console.log(i);
   const [list , setList] = useState(
     [
-      {key:1, title: 'Hello1'},
-      {key:2, title: 'Hello2'},
-      {key:3, title: 'Hello3'},
-      {key:4, title: 'Hello4'},
-      {key:5, title: 'Hello5'},
-      {key:6, title: 'Hello6'},
-      {key:7, title: 'Hello7'},
-      {key:8, title: 'Hello8'},
-      {key:9, title: 'Hello9'},
-      {key:10, title: 'Hello10'}
+      {key:1, title: 'List Item 1'},
+      {key:2, title: 'List Item 2'},
+      {key:3, title: 'List Item 3'},
+      {key:4, title: 'List Item 4'},
+      {key:5, title: 'List Item 5'},
+      {key:6, title: 'List Item 6'},
+      {key:7, title: 'List Item 7'},
+      {key:8, title: 'List Item 8'},
+      {key:9, title: 'List Item 9'},
+      {key:10, title: 'List Item 10'}
     ]
   )
 
   const Del = (swipeData : any, data: any) => {
-    console.log(swipeData)
+    console.log('swipeData = ',swipeData);
     console.log(data.translateX)
     if(data.translateX < 0){
-      const newlist = list;
+      const newlist = [...list];
       const ind = list.findIndex(item => item.key === swipeData);
       newlist.splice(ind,1);
       setList(newlist)
     }
-    console.log(list)
   }
 
   const rndr = (data: any) => {
-    console.log(data)
     return(
       <View>
-          <Text style = {{color: 'white', backgroundColor: 'black', padding: 10, margin: 10, fontSize: 20}}>
-            {data.item.key}{')\b'}{data.item.title}
+          <Text style = {styles.contentText}>
+            {data.item.title}
           </Text>
       </View>
     )
   }
 
+  const rndrHidden = () => {
+    return(
+      <Text style = {styles.deleteText}>
+        Delete
+      </Text>
+    )
+  }
+
   return(
-    <View>
+    <View style = {{backgroundColor: '#8BC34A', height: '100%'}}>
       <SwipeListView
         disableRightSwipe = {true}
         data = {list}
         renderItem = {rndr}
-        renderHiddenItem = {()=><Text style = {{color: 'white', backgroundColor: 'red', padding: 14, margin: 10, textAlign: 'right'}}>Delete</Text>}
+        renderHiddenItem = {rndrHidden}
         swipeGestureEnded = {Del}
         useNativeDriver = {false}
       />
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  deleteText: {
+    color: 'white',
+    backgroundColor: 'red',
+    borderRadius: 10,
+    height: 50, 
+    padding: 14, 
+    margin: 10, 
+    textAlign: 'right'
+  },
+  contentText: {
+    color: 'black',
+    backgroundColor: '#E0E0E0',  
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+    padding: 10, 
+    margin: 10, 
+    fontSize: 20,
+    textAlign: 'center',
+  }
+});
 
 export default App;
